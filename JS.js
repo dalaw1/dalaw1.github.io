6f3f47cb47e5db4fc87bef0ae5c9dd8b6d324b80
Matthew Munger <mattfromthecountry@hotmail.com>
2014-06-04 21:42:30 -0400

    Basics, Tab fade in/out

--- JS.js
+++ JS.js
@@ -1,6 +1,29 @@
 /**
  * Created by matthew on 5/26/2014.
  */
-$('document').ready(function () {
-});
+window.onload = function() {
+    console.log('Javascript Received!');
 
+    var name = document.getElementById('name');
+    var newname = null;
+    var cookie = null;
+    var button = document.getElementById('button');
+
+    if (document.cookie) {
+        name.innerHTML = "Hey " + document.cookie + "! ";
+    }
+    else {
+        name.innerHTML = "";
+    }
+
+    function saveCookie() {
+        newname = document.getElementById('textbox').value;
+        document.cookie = newname;
+        cookie = document.cookie;
+    }
+
+    button.addEventListener("click", function () {
+        saveCookie();
+        name.innerHTML = "Hey " + cookie + "! ";
+    });
+};
