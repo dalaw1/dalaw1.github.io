dd5a1a5540ea3b43b504f0207879806dac36fd19
Matthew Munger <mattfromthecountry@hotmail.com>
2014-06-06 21:09:06 -0400

    all iframes, no functionnal tabs

--- JS.js
+++ JS.js
@@ -2,15 +2,14 @@
  * Created by matthew on 5/26/2014.
  */
 window.onload = function() {
+    // Debug to see if Javascript is Received
     console.log('Javascript Received!');
 
+    // Cookie Manager and Change Greeting Name
     var name = document.getElementById('name');
     var newname = null;
     var cookie = null;
     var button = document.getElementById('button');
-
-    var gamecontainer = document.getElementById('gamecontainer');
-    var currentframe = 0;
 
     if (document.cookie) {
         name.innerHTML = "Hey " + document.cookie + "! ";
@@ -28,23 +27,27 @@ window.onload = function() {
     button.addEventListener("click", function () {
         saveCookie();
         name.innerHTML = "Hey " + cookie + "! ";
+        $(".inputcontainer").fadeTo("slow", 0.5);
     });
 
-    $(document).keydown( function (e) {
+    // Change Iframes In Featured Games
+    var gamecontainer = document.getElementById('gamecontainer');
+    var currentframe = 0;
+
+    $(document).keydown(function (e) {
         if (e.keyCode == 37)
             SlideLeft();
         else if (e.keyCode == 39)
             SlideRight();
     });
-    function SlideLeft()
-    {
+    function SlideLeft() {
         if (currentframe > 0) {
             $("#gamecontainer").animate({"margin-left": "+=500px"});
             currentframe--;
         }
     }
-    function SlideRight()
-    {
+
+    function SlideRight() {
         if (currentframe < 2) {
             $("#gamecontainer").animate({"margin-left": "-=500px"});
             currentframe++;
@@ -52,4 +55,3 @@ window.onload = function() {
 
     }
 };
-
